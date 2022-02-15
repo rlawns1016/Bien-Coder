cfa917e920aceec31b8766c87d20b56cf6afa633
최광혁 <zery.choi@gmail.com>
2022-02-15 12:58:24 +0900

    refactor ISCH from mainframe.h

--- Test-Search/testSearch.cpp
+++ Test-Search/testSearch.cpp
@@ -4,11 +4,17 @@
 
 TEST(SearchCase, SearchByName) {
   
+  //input...
   vector <Employee> employees;
-
   employees.push_back({ "12345678","KIM","GILDONG","CL1","010-2000-0000","000101","ADV" });
+  
+  //SCH »ý¼º
+  ISearch * finder = NaiveSearcher(employees)
+
+
   employees.push_back({ "12345678","HONG","GILDONG","CL2","010-2000-0000","000101","ADV" });
   employees.push_back({ "12345678","CHOI","GILDONG","CL3","010-2000-0000","000101","ADV" });
+
 
   NaiveSearch finder(employees);
   vector <Employee*> result;

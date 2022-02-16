7d08d574ac2d22ddbeebb3d2ff81cb88c98d9a21
김영석 <zs11010@kakao.com>
2022-02-16 10:11:12 +0900

    change data type : enum to string

--- EmployeeManagement/input.cpp
+++ EmployeeManagement/input.cpp
@@ -4,18 +4,18 @@ OP_CODE Instruction::GetOperationCode() {
 	return this->operationCode_;
 }
 
-OPTION_1 Instruction::GetPrintOption() {
-	return this->print_;
+string Instruction::GetOption1() {
+	return this->option1_;
 }
 
-OPTION_2 Instruction::GetCondition() {
-	return this->condition_;
+string Instruction::GetOption2() {
+	return this->option2_;
 }
 
-void InstructionAdd::SetInstruction(const OP_CODE op, const OPTION_1 opt1, const OPTION_2 opt2, const EmployeeInfo& e) {
+void InstructionAdd::SetInstruction(const OP_CODE op, const string opt1, const string opt2, const EmployeeInfo& e) {
 	this->operationCode_ = op;
-	this->print_ = opt1;
-	this->condition_ = opt2;
+	this->option1_ = opt1;
+	this->option2_ = opt2;
 	this->employee_ = e;
 	
 }

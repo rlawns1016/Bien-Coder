284f8d847aecf63949e2fcd1df54a9913aa356e6
최광혁 <zery.choi@gmail.com>
2022-02-17 08:49:51 +0900

    create makeCopyFromParam on DataBase

--- Test_EmployeeManagement/testDataBase.cpp
+++ Test_EmployeeManagement/testDataBase.cpp
@@ -41,5 +41,28 @@ TEST(DBCase, Erase) {
 	EXPECT_EQ(true, db->erase(2002123456));
 	EXPECT_EQ(db->getEmployeeInfo(2002123456), nullptr);
 	EXPECT_EQ(db->getEmployeeCount(), 4);
-	
+}
+
+TEST(DBCase, Search) {
+
+	IDataBase* db = new DataBase();
+	db->add({ 2000123456, {"KIM","GILDONG"},CL::CL1,{1,5678},{1990,1,1},CERTI::ADV });
+	db->add({ 2001123456, {"KIM","GOLDONG"},CL::CL1,{2,5678},{1990,1,1},CERTI::ADV });
+	db->add({ 2002123456, {"HONG","GILDONG"},CL::CL2,{3,5678},{1991,1,1},CERTI::PRO });
+	db->add({ 2003123456, {"KIM","GOLDONG"},CL::CL1,{4,5678},{1990,1,1},CERTI::ADV });
+	db->add({ 2004123456, {"CHOI","GILDONG"},CL::CL3,{5,5678},{1992,1,1},CERTI::EX });
+	vector<unsigned int> pks = db->search("", "name", "CHOI GILDONG");
+
+	EXPECT_EQ(pks.size(), 1);
+	EXPECT_EQ(pks[0], 2004123456);
+}
+
+TEST(DBCase, makeCopy) {
+
+	IDataBase* db = new DataBase();
+	EmployeeInfo info = { 2001123456, {"KIM","GOLDONG"},CL::CL1,{2,5678},{1990,1,1},CERTI::ADV };
+	db->makeCopyFromParam("", "name", "HONG KILDONG", info);
+
+	EXPECT_EQ(info.name.first, "HONG");
+	EXPECT_EQ(info.name.last, "KILDONG");
 }

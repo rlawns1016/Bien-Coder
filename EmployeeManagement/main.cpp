6de872968d079b85d7bc6bfa482f74d4d0683985
rlawns1016 <rlawns10164@gmail.com>
2022-02-17 17:12:34 +0900

    add exception code in main func

--- EmployeeManagement/main.cpp
+++ EmployeeManagement/main.cpp
@@ -1,11 +1,17 @@
 #include "EmployeeManager.h"
 
+#define EXIT_ERROR_CODE_INVALID_INPUT	-1
+
 int main(int argc, char* argv[])
 {
+	if (argc != 3 || !argv[1] || !argv[2])
+	{
+		exit(EXIT_ERROR_CODE_INVALID_INPUT);
+	}
+	int ret = 0;
 	EmployeeManager* manager = new EmployeeManager();
-
-	manager->run(argv[1], argv[2]);
+	ret = manager->run(argv[1], argv[2]);
 	delete manager;
-	return 0;
+	return ret;
 }
 
